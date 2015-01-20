7a3edf1d2c8248578e8dac3d74369a38e62f924c
Uzi Landsmann <uzi.landsmann@arbetsformedlingen.se>
2015-01-20 16:12:42 +0100

    added some book resource stubs

--- src/main/java/com/landsmanns/bookvis/service/BookService.java
+++ src/main/java/com/landsmanns/bookvis/service/BookService.java
@@ -1,8 +1,16 @@
 package com.landsmanns.bookvis.service;
 
+import com.landsmanns.bookvis.model.Book;
+
 /**
  * Created by uzilan on 2015-01-18.
  */
 public interface BookService {
     String getAllBooks();
+
+    Book getBook(String id);
+
+    Book createBook(String name, String authorId, String genreId);
+
+    Book updateBook(String bookId);
 }
